a8d5ba6f3f895666f5556114d396957ef89e6c33
ktjayamanna <kaveen.jayamanna@gmail.com>
2024-11-19 14:35:38 -0500

    154 is up and running

--- BinarySearch/leetcode-154.cpp
+++ BinarySearch/leetcode-154.cpp
@@ -3,39 +3,40 @@ using namespace std;
 
 class Solution {
 public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
 
-int findMin(vector<int>& nums) {
-    int left = 0;
-    int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-    // Edge case: the array is not rotated.
-    if (nums[left] <= nums[right]) {
-        return nums[left];
+            // If the array is already sorted, return the smallest element
+            if (nums[left] <= nums[right]) {
+                return nums[left];
+            }
+
+            // Check if mid is the pivotal point
+            if (mid + 1 < nums.size() && nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+
+            // Check if the current subarray is sorted; stop immediately
+            if (nums[mid] >= nums[left] && nums[mid] <= nums[right]) {
+                return nums[left];
+            }
+
+            // Decide which half to search next
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1; // Pivot is in the right half
+            } else {
+                right = mid - 1; // Pivot is in the left half
+            }
+        }
+
+        // This point is never reached if the input satisfies the problem constraints
+        return nums[0];
     }
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        // Check if mid is the pivot point
-        if (mid < right && nums[mid] > nums[mid + 1]) {
-            return nums[mid + 1];
-        }
-        if (mid > left && nums[mid - 1] > nums[mid]) {
-            return nums[mid];
-        }
-
-        // Decide which side to go
-        if (nums[mid] > nums[right]) {
-            // Minimum is in the right half
-            left = mid + 1;
-        } else {
-            // Minimum is in the left half
-            right = mid - 1;
-        }
-    }
-
-    // After exiting the loop, left should point to the minimum element
-    return nums[left];
-}
-
 };
