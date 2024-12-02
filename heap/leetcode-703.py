5a928fbd8dc8553b8ff253aba94eed083e09ae2e
ktjayamanna <kaveen.jayamanna@gmail.com>
2024-12-02 00:35:25 -0500

    added the 703 working code

--- heap/leetcode-703.py
+++ heap/leetcode-703.py
@@ -3,20 +3,22 @@ from typing import List
 
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
-        self.min_heap = [i for i in nums]
+        self.k = k
+        # Initialize a min-heap with up to k elements
+        self.min_heap = nums
         heapq.heapify(self.min_heap)
-        self.results = [None]
-        #initialize the heap
-        while True:
-            if len(self.min_heap) == k:
-                break
-            if len(self.min_heap) > 0:
-                heapq.heappop(self.min_heap)
-
+        
+        # Reduce the heap size to k if necessary
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
-        if self.min_heap[0]<val:
+        # Add the new value only if it can be in the top k elements
+        if len(self.min_heap) < self.k:
+            heapq.heappush(self.min_heap, val)
+        elif val > self.min_heap[0]:
             heapq.heappop(self.min_heap)
             heapq.heappush(self.min_heap, val)
-        self.results.append(self.min_heap[0])
+        
+        # Return the kth largest element, which is the root of the heap
         return self.min_heap[0]
