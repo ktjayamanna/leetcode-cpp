fd4abe0bd30cb7b959f135ffb41e1da6cb91e10a
ktjayamanna <kaveen.jayamanna@gmail.com>
2024-12-07 13:35:31 -0500

    got rid of the results variable

--- dfs/dfs_traverse_with_path.py
+++ dfs/dfs_traverse_with_path.py
@@ -1,12 +1,33 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
 def all_tree_paths(root):
     def dfs(node, path):
         if not node:
             return []
         path.append(node.val)
+        
         if not node.left and not node.right:
-            result = [path[:]]
-        else:
-            result = dfs(node.left, path) + dfs(node.right, path)
+            # At a leaf node, directly return a copy of the current path
+            leaf_path = [path[:]]
+            path.pop()
+            return leaf_path
+        
+        # Recursively get paths from left and right subtrees
+        left_paths = dfs(node.left, path)
+        right_paths = dfs(node.right, path)
+        
+        # Backtrack after exploring both subtrees
         path.pop()
-        return result
+        
+        # Directly return the combined paths without a separate result variable
+        return left_paths + right_paths
+    
     return dfs(root, [])
+
+
+
+  
