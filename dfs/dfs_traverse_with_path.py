d5e3089016c6cf4ad87361d30be33c8660fd7206
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-01-21 21:27:45 -0500

    function to find the path from the root to the dst

--- dfs/dfs_traverse_with_path.py
+++ dfs/dfs_traverse_with_path.py
@@ -29,5 +29,28 @@ def all_tree_paths(root):
     return dfs(root, [])
 
 
+def get_path(root, val):
+  """
+  Gets the path from the root to a node with the given value, or None if not found.
+
+  Args:
+    root: The root of the binary tree.
+    val: The value of the node to search for.
+
+  Returns:
+    None if the node is not found, otherwise a list of node values from the root to
+    the node.
+  """
+  
+  if not root: return None
+  if root.val == val: return [root.val]
+  left = get_path(root.left, val)
+  if left: return left + [root.val]
+  right = get_path(root.right, val)
+  if right: return right + [root.val]
+  return None
+  
+
+
 
   
