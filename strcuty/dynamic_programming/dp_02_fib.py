967fdeeaccc44da18685b586cb3a18321783a837
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-06 20:55:36 -0400

    adding sum possible and fib

--- strcuty/dynamic_programming/dp_02_fib.py
+++ strcuty/dynamic_programming/dp_02_fib.py
@@ -4,12 +4,21 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function fib that takes in a number argument, n, and returns the n-th number of the Fibonacci sequence.
+
+The 0-th number of the sequence is 0.
+
+The 1-st number of the sequence is 1.
+
+To generate further numbers of the sequence, calculate the sum of previous two numbers.
+
+Solve this recursively.
 
 
 EXAMPLES:
 ================================================================================
-[Add examples here]
+fib(46); # -> 1836311903
+
 
 
 CONSTRAINTS:
@@ -20,4 +29,17 @@ CONSTRAINTS:
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def fib(n):
+  memo = {}
+  return _fib(n, memo)
+
+def _fib(n, memo):
+  if n == 0 or n == 1:
+    return n
+  
+  if n in memo:
+    return memo[n]
+
+  memo[n] = _fib(n - 1, memo) + _fib(n - 2, memo)
+  return memo[n]
+
--- strcuty/dynamic_programming/dp_04_sum_possible.py
+++ strcuty/dynamic_programming/dp_04_sum_possible.py
@@ -4,7 +4,11 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+sum possible
+
+Write a function sum_possible that takes in an amount and a list of positive numbers. The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the list. You may reuse numbers of the list as many times as necessary.
+
+You may assume that the target amount is non-negative.
 
 
 EXAMPLES:
@@ -20,4 +24,24 @@ CONSTRAINTS:
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def sum_possible(amount, numbers):
+  return _sum_possible(amount, numbers, {})
+
+def _sum_possible(amount, numbers, memo):
+  if amount in memo:
+    return memo[amount]
+  
+  if amount < 0:
+    return False
+  
+  if amount == 0:
+    return True
+  
+  for num in numbers:
+    if _sum_possible(amount - num, numbers, memo):
+      memo[amount] = True
+      return True
+    
+  memo[amount] = False  
+  return False
+
