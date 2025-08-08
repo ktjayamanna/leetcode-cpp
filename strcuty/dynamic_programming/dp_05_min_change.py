13d2532a23ad8def91d9572e68d0d4881dd33ae8
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-08 10:25:00 -0400

    adding 1 to diminishing total, DAGs in grid graphs

--- strcuty/dynamic_programming/dp_05_min_change.py
+++ strcuty/dynamic_programming/dp_05_min_change.py
@@ -4,7 +4,9 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function min_change that takes in an amount and a list of coins. The function should return the minimum number of coins required to create the amount. You may use each coin as many times as necessary.
+
+If it is not possible to create the amount, then return -1.
 
 
 EXAMPLES:
@@ -20,4 +22,28 @@ CONSTRAINTS:
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def min_change(amount, coins):
+  ans = _min_change(amount, coins, {})
+  if ans == float('inf'):
+    return -1
+  else:
+    return ans
+
+def _min_change(amount, coins, memo):
+  if amount in memo:
+    return memo[amount]
+  
+  if amount == 0:
+    return 0
+  
+  if amount < 0:
+    return float('inf')
+  
+  min_coins = float('inf')
+  for coin in coins:
+    num_coins = 1 + _min_change(amount - coin, coins, memo)
+    min_coins = min(min_coins, num_coins)
+    
+  memo[amount] = min_coins
+  return min_coins
+
