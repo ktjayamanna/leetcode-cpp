ae92102c04e4536b7996063218dceeee30ec217f
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-10 01:08:28 -0400

    pick myself and pass without me and my neighbor or don't pick me and pass without me

--- strcuty/dynamic_programming/dp_08_non_adjacent_sum.py
+++ strcuty/dynamic_programming/dp_08_non_adjacent_sum.py
@@ -4,12 +4,18 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function, non_adjacent_sum, that takes in a list of numbers as an argument. The function should return the maximum sum of non-adjacent items in the list. 
+There is no limit on how many items can be taken into the sum as long as they are not adjacent.
 
 
 EXAMPLES:
 ================================================================================
-[Add examples here]
+For example, given:
+
+[2, 4, 5, 12, 7]
+
+The maximum non-adjacent sum is 16, because 4 + 12. 
+4 and 12 are not adjacent in the list.
 
 
 CONSTRAINTS:
@@ -20,4 +26,18 @@ CONSTRAINTS:
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def non_adjacent_sum(nums):
+  return _non_adjacent_sum(nums, 0, {})
+
+def _non_adjacent_sum(nums, i, memo):
+  if i in memo:
+    return memo[i]
+  
+  if i >= len(nums):
+    return 0
+  
+  include = nums[i] + _non_adjacent_sum(nums, i + 2, memo)
+  exclude = _non_adjacent_sum(nums, i + 1, memo)
+  memo[i] = max(include, exclude)
+  return memo[i]
+
