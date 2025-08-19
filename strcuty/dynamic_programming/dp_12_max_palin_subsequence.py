b6074c016e546536bc7b53903f4164fecdc8f705
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-19 11:54:05 -0400

    combine two pointer with choose or not choose recursive strategy

--- strcuty/dynamic_programming/dp_12_max_palin_subsequence.py
+++ strcuty/dynamic_programming/dp_12_max_palin_subsequence.py
@@ -4,7 +4,10 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function, max_palin_subsequence, that takes in a string as an argument. 
+The function should return the length of the longest subsequence of the string that is also a palindrome.
+
+A subsequence of a string can be created by deleting any characters of the string, while maintaining the relative order of characters.
 
 
 EXAMPLES:
@@ -16,8 +19,39 @@ CONSTRAINTS:
 ================================================================================
 [Add constraints here]
 
+Complexity:
+================================================================================
+
+    n = length of string
+    Time: O(n^2)
+    Space: O(n^2)
+
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def max_palin_subsequence(string):
+  return _max_palin_subsequence(string, 0, len(string) - 1, {})
+
+def _max_palin_subsequence(string, i, j, memo):
+  key = (i, j)
+  
+  if key in memo:
+    return memo[key]
+  
+  if i == j:
+    return 1
+  
+  if i > j:
+    return 0
+  
+  if string[i] == string[j]:
+    memo[key] = 2 + _max_palin_subsequence(string, i + 1, j - 1, memo)
+  else:
+    memo[key] = max(
+      _max_palin_subsequence(string, i + 1, j, memo),
+      _max_palin_subsequence(string, i, j - 1, memo)
+    )
+    
+  return memo[key]
+
