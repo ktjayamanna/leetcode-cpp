7b046b8ca00bc5296d9d9c75e0ce3b51144fad23
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-20 11:13:00 -0400

    decide which pointer needs to be updated in order to get the sub sequence. Then take the max of possibilities

--- strcuty/dynamic_programming/dp_13_overlap_subsequence.py
+++ strcuty/dynamic_programming/dp_13_overlap_subsequence.py
@@ -4,7 +4,9 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function, overlap_subsequence, that takes in two strings as arguments. 
+The function should return the length of the longest overlapping subsequence.
+A subsequence of a string can be created by deleting any characters of the string, while maintaining the relative order of characters.
 
 
 EXAMPLES:
@@ -16,8 +18,38 @@ CONSTRAINTS:
 ================================================================================
 [Add constraints here]
 
+COMPLEXITY:
+================================================================================
+
+    n = length of string_1
+    m = length of string_2
+    Time: O(n*m)
+    Space: O(n*m)
+
 """
+
+
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+
+def overlap_subsequence(string_1, string_2):
+  return _overlap_subsequence(string_1, string_2, 0, 0, {})
+
+def _overlap_subsequence(string_1, string_2, i, j, memo):
+  key = (i, j)
+  if key in memo:
+    return memo[key]
+  
+  if i == len(string_1) or j == len(string_2):
+    return 0
+  
+  if string_1[i] == string_2[j]:
+    memo[key] = 1 + _overlap_subsequence(string_1, string_2, i + 1, j + 1, memo)
+  else:
+    memo[key] = max(
+      _overlap_subsequence(string_1, string_2, i + 1, j, memo),
+      _overlap_subsequence(string_1, string_2, i, j + 1, memo)
+    )
+  return memo[key]
+
