9fa5701e61ad0de9f3d019834d28be6006581d00
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-21 11:58:34 -0400

    apply permutations pattern match the subsequence to the array elements

--- strcuty/dynamic_programming/dp_14_can_concat.py
+++ strcuty/dynamic_programming/dp_14_can_concat.py
@@ -4,7 +4,10 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function, can_concat, that takes in a string and a list of words as arguments. 
+The function should return boolean indicating whether or not it is possible to concatenate words of the list together to form the string.
+
+You may reuse words of the list as many times as needed.
 
 
 EXAMPLES:
@@ -16,8 +19,35 @@ CONSTRAINTS:
 ================================================================================
 [Add constraints here]
 
+COMPLEXITY:
+================================================================================
+
+s = length of string
+w = # of words
+Time: ~O(sw)
+Space: O(s)
+
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def can_concat(s, words):
+  return _can_concat(s, words, 0, {})
+
+def _can_concat(s, words, i, memo):
+  if i in memo:
+    return memo[i]
+  
+  if i == len(s):
+    return True
+  
+  for word in words:
+    if s.startswith(word, i):
+      if _can_concat(s, words, i + len(word), memo) == True:
+        memo[i] = True
+        return True
+      
+  memo[i] = False
+  return False
+
+
