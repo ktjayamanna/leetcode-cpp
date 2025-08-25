ea2689b24d165c0cb813eb3b7ae97c60bf0af1ba
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-25 10:16:45 -0400

    find the min of permutations of an array

--- strcuty/dynamic_programming/dp_15_quickest_concat.py
+++ strcuty/dynamic_programming/dp_15_quickest_concat.py
@@ -4,7 +4,9 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+Write a function, quickest_concat, that takes in a string and a list of words as arguments. The function should return the minimum number of words needed to build the string by concatenating words of the list. If it is not possible to build the string, then return -1.
+
+You may use words of the list as many times as needed.
 
 
 EXAMPLES:
@@ -16,8 +18,39 @@ CONSTRAINTS:
 ================================================================================
 [Add constraints here]
 
+COMPLEXITY:
+================================================================================
+
+s = length of string
+w = # of words
+Time: ~O(sw)
+Space: O(s)
+
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def quickest_concat(s, words):
+  result = _quickest_concat(s, words, 0, {})
+  if result == float('inf'):
+    return -1
+  else:
+    return result
+
+def _quickest_concat(s, words, i, memo):
+  if i in memo:
+    return memo[i]
+  
+  if i == len(s):
+    return 0
+  
+  min_words = float('inf')
+  for word in words:
+    if s.startswith(word, i):
+      attempt = 1 + _quickest_concat(s, words, i + len(word), memo)
+      min_words = min(attempt, min_words)
+  
+  memo[i] = min_words
+  return min_words
+
+
