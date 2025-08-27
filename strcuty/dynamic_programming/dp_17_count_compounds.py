5a5c4a77eb4b9d4221006d7e65ba6902fcbc4de1
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-08-27 10:16:03 -0400

    enumerate the array elements and use a summation logic

--- strcuty/dynamic_programming/dp_17_count_compounds.py
+++ strcuty/dynamic_programming/dp_17_count_compounds.py
@@ -4,7 +4,13 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-[Add problem description here]
+You are a chemist and have to figure out how many ways you can make a given compound.
+
+Write a function, count_compounds, that takes in a target compound and a list of elements. The function should return the number of ways we can make the compound with the given elements.
+
+A compound is made by concatenating one or more elements together.
+
+You may reuse elements of the list as many times as needed to make a compound.
 
 
 EXAMPLES:
@@ -16,8 +22,35 @@ CONSTRAINTS:
 ================================================================================
 [Add constraints here]
 
+COMPLEXITY:
+================================================================================
+
+c = length of compound
+e = # of elements
+Time: O(cw)
+Space: O(c)
+
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def count_compounds(compound, elements):
+  return _count_compounds(compound, elements, 0, {})
+
+def _count_compounds(compound, elements, idx, memo):
+  if idx == len(compound):
+    return 1
+
+  if idx in memo:
+    return memo[idx]
+
+  count = 0
+  for ele in elements:
+    if compound.startswith(ele.lower(), idx):
+      count += _count_compounds(compound, elements, idx + len(ele), memo)
+
+  memo[idx] = count
+  return count
+
+
+
