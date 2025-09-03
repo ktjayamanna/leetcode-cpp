3a41b8e7f670c7b03876becdf4347ac216c57e92
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-09-03 10:10:02 -0400

    keep track of the current idx and previous sum and replace the current idx with the total you have seen so far

--- strcuty/strings_and_arrays/sa_01_running_sum.py
+++ strcuty/strings_and_arrays/sa_01_running_sum.py
@@ -4,7 +4,11 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, running_sum, that takes in a list of numbers. 
+The function should return a new list of the same length where each element contains the running sum up to that index of the original list.
+For example, the i-th result should be the sum of all elements 0 to i:
 
+result[i] = numbers[0] + numbers[1] + numbers[2] + ... + numbers[i]
 
 EXAMPLES:
 ================================================================================
@@ -12,10 +16,19 @@ EXAMPLES:
 
 COMPLEXITY:
 ================================================================================
-
+n = length of list
+Time: O(n)
+Space: O(n)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def running_sum(numbers):
+  total = 0
+  result = []
+  for num in numbers:
+    total += num
+    result.append(total)
+  return result
+
