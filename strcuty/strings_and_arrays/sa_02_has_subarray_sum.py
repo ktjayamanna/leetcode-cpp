eadc1f18d8f93cc5419cf305213479f84499cc37
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-09-15 17:16:41 -0400

    create a prefix arr, take a and b factors, if the complement doesn't exist in a seen list, add them, iterate until you find the complement in the seen list

--- strcuty/strings_and_arrays/sa_02_has_subarray_sum.py
+++ strcuty/strings_and_arrays/sa_02_has_subarray_sum.py
@@ -4,18 +4,38 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, has_subarray_sum, that takes in an list of numbers and a target_sum. 
+The function should return a boolean indicating whether or not there exists a subarray of numbers that sums to the given target.
 
+A subarray is a consecutive series of one or more elements of the list.
 
 EXAMPLES:
 ================================================================================
+has_subarray_sum([1, 3, 1, 4, 3], 8) # -> True
 
 
 COMPLEXITY:
 ================================================================================
-
+n = length of numbers
+Time: O(n)
+Space: O(n)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def has_subarray_sum(numbers, target_sum):
+  prefix_sums = [0]
+  total = 0
+  for num in numbers:
+    total += num
+    prefix_sums.append(total)
+
+  seen = set()
+  for current in prefix_sums:
+    complement = current - target_sum
+    if complement in seen:
+      return True
+    seen.add(current)
+  return False
+
