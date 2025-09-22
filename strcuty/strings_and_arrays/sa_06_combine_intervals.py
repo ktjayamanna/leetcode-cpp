f42feb5bbfde363cc4bf84b4282239236f69bdbd
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-09-22 09:31:24 -0400

    take the previous interval and current interval after sorting asc for the starting time. If the ending of prev end time is larger than the starting time of the current, they need to be merged. Otherwise, add the current as a new interval to the result. While merging, if the current ending is larger than the prev ending, updated interval has to be (prev_start, current_end)

--- strcuty/strings_and_arrays/sa_06_combine_intervals.py
+++ strcuty/strings_and_arrays/sa_06_combine_intervals.py
@@ -4,18 +4,57 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, combine_intervals, that takes in a list of intervals as an argument. 
+Each interval is a tuple containing a pair of numbers representing a start and end time. 
+Your function should combine overlapping intervals and return a list containing the combined intervals.
+
+
 
 
 EXAMPLES:
 ================================================================================
+For example:
 
+Given two intervals:
+
+(1, 4) and (3, 7)
+
+The intervals overlap and
+should be combined into:
+
+(1, 7)
+
+You may return the combined intervals in any order.
+
+You can assume that the input list contains at least one interval and all intervals are valid with start < end.
 
 COMPLEXITY:
 ================================================================================
+
+    n = number of intervals
+    Time: O(nlogn)
+    Space: O(n)
 
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+
+def combine_intervals(intervals):
+  sorted_intervals = sorted(intervals, key=lambda x : x[0])
+  combined = [ sorted_intervals[0] ]
+  
+  for current_interval in sorted_intervals[1:]:
+    last_start, last_end = combined[-1]
+    current_start, current_end = current_interval
+    
+    if current_start <= last_end:
+      if current_end > last_end:
+        combined[-1] = (last_start, current_end)
+    else:
+      combined.append(current_interval)
+      
+  return combined
+  
+
