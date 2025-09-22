0a81c34a081ab838785f650972def2e876828d5b
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-09-22 09:22:12 -0400

    recursively split the array into left and right until you are left with individual elements, then at each node, loop over left and and right children and return a sorted combined array leveraging their sortedness

--- strcuty/strings_and_arrays/sa_05_merge_sort.cpp
+++ strcuty/strings_and_arrays/sa_05_merge_sort.cpp
@@ -4,10 +4,14 @@
 /*
 PROBLEM DESCRIPTION:
 ================================================================================
-
+Write a function, merge_sort, that takes in a list of numbers as an argument. 
+The function should return a new list containing elements of the original list sorted in ascending order. Your function must implement the merge sort algorithm.
 
 EXAMPLES:
 ================================================================================
+numbers = [10, 4, 42, 5, 8, 100, 5, 6, 12, 40]
+merge_sort(numbers)
+# -> [ 4, 5, 5, 6, 8, 10, 12, 40, 42, 100 ] 
 
 
 COMPLEXITY:
--- strcuty/strings_and_arrays/sa_05_merge_sort.py
+++ strcuty/strings_and_arrays/sa_05_merge_sort.py
@@ -4,18 +4,47 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-
+Write a function, merge_sort, that takes in a list of numbers as an argument. 
+The function should return a new list containing elements of the original list sorted in ascending order. Your function must implement the merge sort algorithm.
 
 EXAMPLES:
 ================================================================================
-
+numbers = [10, 4, 42, 5, 8, 100, 5, 6, 12, 40]
+merge_sort(numbers)
+# -> [ 4, 5, 5, 6, 8, 10, 12, 40, 42, 100 ] 
 
 COMPLEXITY:
 ================================================================================
+    n = list size
+    Time: O(nlogn)
+    Space: O(n)
 
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+from collections import deque
+
+def merge_sort(nums):
+  if len(nums) <= 1:
+    return nums  
+  mid_idx = len(nums) // 2
+  left_sorted = merge_sort(nums[:mid_idx])
+  right_sorted = merge_sort(nums[mid_idx:])  
+  return merge(left_sorted, right_sorted)
+
+def merge(list_1, list_2):
+  list_1 = deque(list_1)
+  list_2 = deque(list_2)
+  
+  merged = []
+  while list_1 and list_2:
+    if list_1[0] < list_2[0]:
+      merged.append(list_1.popleft())
+    else:
+      merged.append(list_2.popleft())
+  merged += list_1
+  merged += list_2
+  return merged
+
