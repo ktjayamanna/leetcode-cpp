0413b02c40d360158760bbdc1a5c9176bcc86394
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-09-29 11:08:20 -0400

    have low and high pointer, each iteration calc mid point and check if mid point is the target, if not update the pointers relative to the mid point that we have currently calculated

--- strcuty/strings_and_arrays/sa_07_binary_search.py
+++ strcuty/strings_and_arrays/sa_07_binary_search.py
@@ -4,18 +4,37 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, binary_search, that takes in a sorted list of numbers and a target. The function should return the index where the target can be found within the list. If the target is not found in the list, then return -1.
 
+You may assume that the input array contains unique numbers sorted in increasing order.
+
+Your function must implement the binary search algorithm.
 
 EXAMPLES:
 ================================================================================
-
+binary_search([0, 1, 2, 3, 4, 5, 6, 7, 8], 6) # -> 6
+binary_search([0, 6, 8, 12, 16, 19, 20, 24, 28], 27) # -> -1
 
 COMPLEXITY:
 ================================================================================
-
+n = length of numbers array
+Time: O(logn)
+Space: O(1)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def binary_search(numbers, target):
+  lo = 0
+  hi = len(numbers) - 1
+  while lo <= hi:
+    mid = (lo + hi) // 2
+    if target < numbers[mid]:
+      hi = mid - 1
+    elif target > numbers[mid]:
+      lo = mid + 1
+    else:
+      return mid
+  return -1
+
