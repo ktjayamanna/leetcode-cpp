434ad4b6de408294a3982442afe3d254ef4b8938
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-10-02 21:50:01 -0400

    have a function that lexically compare 2 words, then apply that function to the array, two words at a time. If there is at least one pair that is not sorted, return false. Otherwise, late return True

--- strcuty/strings_and_arrays/sa_09_detect_dictionary.py
+++ strcuty/strings_and_arrays/sa_09_detect_dictionary.py
@@ -4,18 +4,50 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, detectDictionary, that takes in a dictionary of words and an alphabet string. The function should return a boolean indicating whether or not all words of the dictionary are lexically-ordered according to the alphabet.
 
+You can assume that all characters are lowercase a-z.
+
+You can assume that the alphabet contains all 26 letters.
 
 EXAMPLES:
 ================================================================================
+dictionary = ["zoo", "tick", "tack", "door"]
+alphabet = "ghzstijbacdopnfklmeqrxyuvw"
+detect_dictionary(dictionary, alphabet) # -> True
+
+dictionary = ["zoo", "tick", "tack", "door"]
+alphabet = "ghzstijbacdopnfklmeqrxyuvw"
+detect_dictionary(dictionary, alphabet) # -> True
 
 
 COMPLEXITY:
 ================================================================================
-
+n = # of words in dictionary
+k = # length of longest word
+Time: O(nk)
+Space: O(1)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def detect_dictionary(dictionary, alphabet):
+  for i in range(0, len(dictionary) - 1):
+    current = dictionary[i]
+    next = dictionary[i + 1]
+    if not lexical_order(current, next, alphabet):
+      return False  
+  return True
+
+def lexical_order(word_1, word_2, alphabet):
+  length = max(len(word_1), len(word_2))
+  for i in range(length): 
+    value_1 = alphabet.index(word_1[i]) if i < len(word_1) else float('-inf')
+    value_2 = alphabet.index(word_2[i]) if i < len(word_2) else float('-inf')
+    if value_1 < value_2:
+      return True
+    elif value_2 < value_1:
+      return False
+  return True
+
