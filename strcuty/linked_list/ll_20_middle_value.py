1f715712edc7b982ea4f2ce4f8e169fa6180f2d1
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-10-04 10:14:57 -0400

    take two ptrs, one slow and one twice as fast, and slower should hit the middle when the twice as fast hit the end. Draw and derive the while loop conditions

--- strcuty/linked_list/ll_20_middle_value.py
+++ strcuty/linked_list/ll_20_middle_value.py
@@ -4,18 +4,62 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, middle_value, that takes in the head of a linked list as an argument. 
+The function should return the value of the middle node in the linked list. 
+If the linked list has an even number of nodes, then return the value of the second middle node.
 
+You may assume that the input list is non-empty.
 
 EXAMPLES:
 ================================================================================
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+# a -> b -> c -> d -> e
+middle_value(a) # c
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+# a -> b -> c -> d -> e -> f
+middle_value(a) # d
 
 
 COMPLEXITY:
 ================================================================================
-
+n = number of nodes
+Time: O(n)
+Space: O(1)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def middle_value(head):
+  slow = head
+  fast = head
+  
+  while fast is not None and fast.next is not None:
+    slow = slow.next
+    fast = fast.next.next
+    
+  return slow.val
+
