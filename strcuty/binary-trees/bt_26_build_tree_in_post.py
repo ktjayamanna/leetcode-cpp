148abd7eef54df46d3cc6034148fbd9f7e1ff91a
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-10-26 09:25:26 -0400

    grab root as the last ele of the post order, then pick the left side and the right side of in order list for the sub trees of the in order of the root. Grab the left node of the post order by taking the first element, then pass the same num of elements as the corresponding in order sequence. Make sure to skip the last ele of the post order to avoid double counting the root

--- strcuty/binary-trees/bt_26_build_tree_in_post.py
+++ strcuty/binary-trees/bt_26_build_tree_in_post.py
@@ -4,18 +4,67 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, build_tree_in_post, that takes in a list of in-ordered values and a list of post-ordered values for a binary tree. 
+The function should build a binary tree that has the given in-order and post-order traversal structure. 
+The function should return the root of this tree.
 
+You can assume that the values of inorder and postorder are unique.
 
 EXAMPLES:
 ================================================================================
 
+build_tree_in_post(
+  [ 'd', 'b', 'g', 'e', 'h', 'a', 'c', 'f' ],
+  [ 'd', 'g', 'h', 'e', 'b', 'f', 'c', 'a' ] 
+)
+#      a
+#    /    \
+#   b      c
+#  / \      \
+# d   e      f
+#    / \
+#    g  h
+
+build_tree_in_post(
+  [ 'd', 'b', 'e', 'a', 'f', 'c', 'g' ],
+  [ 'd', 'e', 'b', 'f', 'g', 'c', 'a' ] 
+)
+#      a
+#    /    \
+#   b      c
+#  / \    / \
+# d   e  f   g
+
 
 COMPLEXITY:
 ================================================================================
-
+n = length of array
+Time: O(n^2)
+Space: O(n^2)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+
+def build_tree_in_post(in_order, post_order):
+  if len(in_order) == 0:
+    return None
+  
+  value = post_order[-1]
+  root = Node(value)
+  mid = in_order.index(value)
+  left_in = in_order[:mid]
+  right_in = in_order[mid + 1:]  
+  left_post = post_order[:len(left_in)]
+  right_post = post_order[len(left_in):-1]
+  root.left = build_tree_in_post(left_in, left_post)
+  root.right = build_tree_in_post(right_in, right_post)
+  return root
+
