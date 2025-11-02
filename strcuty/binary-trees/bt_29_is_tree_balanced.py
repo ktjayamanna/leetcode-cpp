f28f60152808e78d00bb9ed0aed9f1393d0ecaab
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-11-02 10:47:10 -0500

    encode tree height with standard definition and encode impossible case with + inf and let the max logic take care of propogation

--- strcuty/binary-trees/bt_29_is_tree_balanced.py
+++ strcuty/binary-trees/bt_29_is_tree_balanced.py
@@ -4,18 +4,93 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, is_tree_balanced, that takes in the root of a binary tree. 
+The function should return a boolean indicating whether or not the tree is "balanced".
 
+A "balanced" binary tree is a binary tree where the height between the left and right subtrees differs by at most 1 for every node.
 
 EXAMPLES:
 ================================================================================
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.left = b
+a.right = c
+b.right = d
+
+#         a
+#       /   \
+#      b    c
+#      \
+#      d
+
+is_tree_balanced(a) # -> True
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+
+a.right = b
+b.right = c
+
+#        a
+#         \
+#          b
+#          \
+#           c
+
+is_tree_balanced(a) # -> False
 
 
 COMPLEXITY:
 ================================================================================
-
+n = number of nodes
+Time: O(n)
+Space: O(n)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def check_height_balance(root):
+    if root is None:
+        return 0
+
+    left_height = check_height_balance(root.left)
+    if left_height == -1:
+        return -1
+
+    right_height = check_height_balance(root.right)
+    if right_height == -1:
+        return -1
+
+    if abs(left_height - right_height) > 1:
+        return -1
+    else:
+        return 1 + max(left_height, right_height)
+
+def is_tree_balanced(root):
+    return check_height_balance(root) > -1
+# My answer which is more aligned with the definition of the height of a tree.
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def _is_tree_balanced(root):
+  if not root:
+    return -1
+  left = _is_tree_balanced(root.left)
+  right = _is_tree_balanced(root.right)
+  if abs(left - right) > 1:
+    return float('inf')
+  else:
+    return 1 + max(left, right)
+
+def is_tree_balanced(root):
+  return False if _is_tree_balanced(root) == float('inf') else True
+
+    
