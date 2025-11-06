b26715cb1532c4f7d6a1afef3b3a5cc9974e8bc6
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-11-06 16:44:16 -0500

    take a min heap and keep candidate the k largest elements. Kick out the root every time you find a val larger than the root. After exploring the array, return the root, which is the smallest but also the kth largest

--- strcuty/heap/h_04_kth_largest.py
+++ strcuty/heap/h_04_kth_largest.py
@@ -4,19 +4,37 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-
+Write a function, kth_largest, that takes in a list of numbers and a value, k. 
+The function should return the k-th largest element of the list.
 
 EXAMPLES:
 ================================================================================
+kth_largest([9,2,6,6,1,5,8,7], 3) # -> 7
 
+numbers = [  
+  4,5,85,77,47,80,37,42,3,6,62,33,69,68,16,20,83,39,14,58,75,35,72,36,19,18,66,61,41,79,28,43,7,24,40,53,32,12
+]
+kth_largest(numbers, 9) # -> 68
 
 COMPLEXITY:
 ================================================================================
-
+n = length of list
+k = k-value
+Time: O(n*log(k))
+Space: O(k)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+import heapq
+
+def kth_largest(numbers, k):
+  heap = []
+  for num in numbers:
+    heapq.heappush(heap, num)
+    if len(heap) > k:
+      heapq.heappop(heap)
+  return heapq.heappop(heap)
+
 
