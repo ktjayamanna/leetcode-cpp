7d7b57c2537f8debcee74d763672addd1bb289d1
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-11-09 16:17:25 -0500

    retain k largest values in a max heap, if the candidate is smaller than the root, kick out the root and insert the candiate to the heap

--- strcuty/heap/h_06_k_smallest.py
+++ strcuty/heap/h_06_k_smallest.py
@@ -4,19 +4,52 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
-
+Write a function that takes in a list of numbers and a value, k. 
+The function should return the k smallest numbers in the list. 
+The resulting list should be ordered from least to greatest.
 
 EXAMPLES:
 ================================================================================
+k_smallest([84, 22, 52, 69, 71, 22, 88, 100, 13, 89, 79], 4) 
+# -> [13, 22, 22, 52]
+
+k_smallest([
+  43, 35, 62, 31, 86, 81, 58, 80, 91, 13, 54, 78, 
+  75, 69, 60, 8, 22, 12, 30, 79, 100, 2, 64, 57, 
+  11, 55, 7, 68, 66, 14, 45, 26, 83, 24, 28, 76, 
+  34, 89, 37, 32, 41, 88, 20, 82, 59, 4, 40, 9, 
+  74, 23
+], 5)
+# -> [2, 4, 7, 8, 9]
 
 
 COMPLEXITY:
 ================================================================================
-
+n = length of list
+k = k-value
+Time: O(n*log(k))
+Space: O(k)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+import heapq
+
+def k_smallest(nums, k):
+  max_heap = []
+
+  for v in nums:
+    item = (-v, v)
+    heapq.heappush(max_heap, item)
+    if len(max_heap) > k:
+      heapq.heappop(max_heap)
+
+  result = []
+  while len(max_heap) > 0:
+    item = heapq.heappop(max_heap)
+    result.append(item[1])
+      
+  return result[::-1]
+
 
