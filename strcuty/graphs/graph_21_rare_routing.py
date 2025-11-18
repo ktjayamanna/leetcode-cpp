859c647288154cc3b7d0d1c185808e05b948bae9
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-11-18 09:58:46 -0500

    remember the last node and return false if you revisit a visited node. Also, if the visited set doesn't cover all the nodes, also return False

--- strcuty/graphs/graph_21_rare_routing.py
+++ strcuty/graphs/graph_21_rare_routing.py
@@ -4,19 +4,80 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, rare_routing, that takes in a number of cities (n) and a list of tuples where each tuple represents a direct road that connects a pair of cities. 
+The function should return a boolean indicating whether or not there exists a unique route for every pair of cities. A route is a sequence of roads that does not visit a city more than once.
 
+Cities will be numbered 0 to n - 1.
+
+You can assume that all roads are two-way roads. This means if there is a road between A and B, then you can use that road to go from A to B or go from B to A.
 
 EXAMPLES:
 ================================================================================
+For example, given these roads:
+
+0 --- 1
+| \
+|  \
+|   \
+2    3
+
+There is a unique route for between every pair of cities.
+So the answer is True.
+
+
+For example, given these roads:
+
+0 --- 1
+| \
+|  \
+|   \
+2 -- 3
+
+There are two routes that can be used to travel from city 1 to city 2:
+- first route:  1, 0, 2
+- second route: 1, 0, 3, 2 
+The answer is False, because routes should be unique.
 
 
 COMPLEXITY:
 ================================================================================
-
+n = number of nodes
+Time: O(n^2)
+Space: O(n^2)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def rare_routing(n, roads):
+  graph = make_graph(n, roads)
+  visited = set()
+  valid = validate(graph, 0, visited, None)
+  return valid and len(visited) == n
+
+
+def validate(graph, node, visited, last_node):
+  if node in visited:
+    return False
+  
+  visited.add(node)
+  for neighbor in graph[node]:
+    if neighbor != last_node and not validate(graph, neighbor, visited, node):
+      return False
+    
+  return True
+
+def make_graph(n, roads):
+  graph = {}
+  
+  for city in range(n):
+    graph[city] = []
+    
+  for road in roads:
+    a, b = road
+    graph[a].append(b)
+    graph[b].append(a)
+    
+  return graph
+
 
