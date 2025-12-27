cf2b5b1f8c44cb378fc164f048703d4e3b6d75c4
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-12-27 12:06:40 -0500

    recurse and get the result without the first. Then, append the first to each index of the each permutation

--- strcuty/exhaustive_recursion/er_03_permutations/er_03_permutations.py
+++ strcuty/exhaustive_recursion/er_03_permutations/er_03_permutations.py
@@ -4,19 +4,50 @@
 """
 PROBLEM DESCRIPTION:
 ================================================================================
+Write a function, permutations, that takes in a list an argument. 
 
+The function should return a 2D list where each sublist represents one of the possible permutations of the list.
+
+The sublists may be returned in any order.
+
+You may assume that the input list contains unique items.
 
 EXAMPLES:
 ================================================================================
 
+permutations(['a', 'b', 'c']) # -> 
+# [ 
+#   [ 'a', 'b', 'c' ], 
+#   [ 'b', 'a', 'c' ], 
+#   [ 'b', 'c', 'a' ], 
+#   [ 'a', 'c', 'b' ], 
+#   [ 'c', 'a', 'b' ], 
+#   [ 'c', 'b', 'a' ] 
+# ] 
+
 
 COMPLEXITY:
 ================================================================================
-
+n = length of elements array
+Time: ~O(n!)
+Space: ~O(n!)
 
 """
 
 # SOLUTION:
 # ================================================================================
-# [Add your Python solution here]
+def permutations(items):
+  if not items:
+    return [[]]
+  
+  first = items[0]
+  remaining = items[1:]
+  perms = permutations(remaining)
+  result = []
+  for perm in perms:
+    for i in range(len(perm) + 1):
+      result.append([*perm[:i], first, *perm[i:]])
+    
+  return result
+
 
